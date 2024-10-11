54a56477a87947f078c31538c6b456935950b9c0
MichaelOtieno <okellofrost@gmail.com>
2024-10-11 15:40:36 +0300

    finishing up on tab code in Maincativity

--- app/src/main/java/com/example/android/fragmments/MainActivity.java
+++ app/src/main/java/com/example/android/fragmments/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -28,5 +30,35 @@ public class MainActivity extends AppCompatActivity {
         frameLayout = findViewById(R.id.framelayout);
 
         tablayout = findViewById(R.id.tabLayout);
+        // Default Values
+        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new FirstFragment())
+                .addToBackStack(null).commit();
+
+        tablayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment fragment = null;
+                switch(tab.getPosition()){
+                    case 0 : fragment = new FirstFragment();
+                    break;
+                    case 1 : fragment = new SecondFragment();
+                    break;
+                    case 2 : fragment = new ThirdFragment();
+                    break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,fragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
